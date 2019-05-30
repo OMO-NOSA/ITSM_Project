fc0bae341be6c8e2fcd26cfd17d25411d5c20927
japhet Omorodion <japhet.omorodion@gmail.com>
2019-05-30 17:39:42 +0100

    Added a foreign key to the user model

--- app/tracks/models.py
+++ app/tracks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Track(models.Model):
@@ -6,6 +7,7 @@ class Track(models.Model):
     description = models.TextField(blank=True)
     url = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
+    posted_by = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE)
 
 
     
